22be77b00ecbc89f8cf83290a404a0f9227a8885
emersonnobre <egrnobre@gmail.com>
2022-04-15 21:52:45 -0400

    backend deploy ajusts

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 mongoose = require("mongoose");
 
 module.exports = mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(`mongodb+srv://emersonnobrer:${process.env.MONGO_URI}@cluster0.srcqf.mongodb.net/node-angular?retryWrites=true&w=majority`)
     .then(() => console.log("Connected to the db"))
     .catch(console.log);
